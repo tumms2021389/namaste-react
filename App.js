e17e0e28c1f4aaa288bd9851f8c5fca95e36fd1e
Siva Rama Krishna <sivaramakrishna.tumma@in.pega.com>
2023-07-30 15:12:10 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+const parent = React.createElement('div', { id: 'parent' }, [
+  React.createElement('div', { id: 'child' }, [
+    React.createElement('h1', {}, 'I am a H1 tag'),
+    React.createElement('h2', {}, 'I am a H2 tag'),
+  ]),
+  React.createElement('div', { id: 'child2' }, [
+    React.createElement('h1', {}, 'I am a H1 tag'),
+    React.createElement('h2', {}, 'I am a H2 tag'),
+  ]),
+]);
+// const heading = React.createElement('h1', { id: 'heading' }, 'Hello World from React');
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(parent);
